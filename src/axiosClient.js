75e02a3ff353a98b5a30f366c2d1d56031fd07c8
vthapnghia <103355716+vthapnghia@users.noreply.github.com>
2023-06-30 22:20:22 +0700

    update 30062023

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://khoa.hosting.xuantan97.com/",
 });
 
 // Add a request interceptor
